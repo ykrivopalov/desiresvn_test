c31c6c1b303836b30b4a08c6fffaa64538e118ee
Yury Krivopalov <ykrivopalov@yandex.ru>
2015-07-20 23:45:20 +0300

    add documentation for eval library

--- eval/integrator.h
+++ eval/integrator.h
@@ -11,21 +11,36 @@ typedef std::pair<double, double> Point;
 typedef std::function<double(double)> Function;
 typedef std::function<void(double)> ResultHandler;
 
+/// @brief class for async execution control
 class Execution {
  public:
+  /// @brief synchronously wait until execution finished
   virtual void Wait() = 0;
+
+  /// @brief send cancel signal to execution, doesn't wait
   virtual void Cancel() = 0;
 };
 
 typedef std::shared_ptr<Execution> ExecutionPtr;
 
+/// @brief class for asynchronous calculation of definite integral by trapesoid
+/// rule
 class Integrator {
  public:
   Integrator();
 
-  ExecutionPtr Integrate(Function f, double x0, double x1, ResultHandler handler);
+  /// @brief Run integration process
+  /// @param f is function for integration
+  /// @param x0 is left border of integration
+  /// @param x1 is right border of integration
+  /// @param handler is result of integration consumer
+  ExecutionPtr Integrate(Function f, double x0, double x1,
+                         ResultHandler handler);
 
+  /// @brief set step of integration
   void set_integration_step(double step) { integration_step_ = step; }
+
+  /// @brief set count of threads that used for integration
   void set_threads_count(std::size_t count) { thread_count_ = count; }
 
  private:
@@ -33,6 +48,7 @@ class Integrator {
   std::size_t thread_count_;
 };
 
+/// @brief integrate with Integrator, but synchronously
 double IntegrateSync(eval::Function f, double x0, double x1, double step,
-                    std::size_t thread_count);
+                     std::size_t thread_count);
 }  // namespace eval
--- eval/thread_pool.h
+++ eval/thread_pool.h
@@ -7,17 +7,22 @@ namespace eval {
 
 typedef std::function<void()> Routine;
 
+/// @brief interface for thread pool management
 class ThreadPool {
  public:
   ThreadPool() = default;
   ThreadPool(const ThreadPool& obj) = delete;
   virtual ~ThreadPool() {}
 
+  /// @brief execute routine in separate thread
   virtual void Execute(Routine routine) = 0;
 };
 
 typedef std::shared_ptr<ThreadPool> ThreadPoolPtr;
 
+/// @brief Creates thread pool that manages specified threads number.
+/// @note All the routines are executed in preallocated threads. If the number
+/// of threads is not enough then routine will be put in the queue.
 ThreadPoolPtr CreateThreadPool(std::size_t thread_count);
 
 }
